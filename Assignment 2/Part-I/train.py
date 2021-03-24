cd64f9ad36985d332e14f20dae3fbb49c0c818b2
Mansi <mansimittal027@gmail.com>
2021-03-24 21:25:26 +0000

    testing testing

--- Assignment 2/Part-I/train.py
+++ Assignment 2/Part-I/train.py
@@ -219,7 +219,7 @@ def trainIters(trainloader,encoder, decoder, bridge,device,bidirectional=False,t
 
             #Assign the data to training_pair
             training_pair = data #CODE_BLANK_2
-
+            
             # Assign the input tensor 
             input_tensor = training_pair[0] #CODE_BLANK_3
 
@@ -237,7 +237,7 @@ def trainIters(trainloader,encoder, decoder, bridge,device,bidirectional=False,t
             
             #The train function returns the loss for each batch
             #Call the train function and fill in the arguments from above
-            loss = train( input_tensor, target_tensor, mask_input, mask_target, encoder, decoder, bridge, encoder_optimizer, decoder_optimizer, bridge_optimizer,device, criterion, max_length,bidirectional=False,teacher_forcing=True) #Enter your arguments here, Dont change the below arguments
+            loss = train(input_tensor, target_tensor, mask_input, mask_target, encoder, decoder, bridge, encoder_optimizer, decoder_optimizer, bridge_optimizer,device, criterion, max_length,bidirectional=False,teacher_forcing=True) #Enter your arguments here, Dont change the below arguments
             
             #Printing losses every few iterations
             print_loss_total += loss
